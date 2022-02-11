a75a4995f919db45e5471e9849ee5949d8643853
Alan Wake <67761888+aw-c@users.noreply.github.com>
2022-02-11 11:28:20 +0300

    Create clearshit.lua

---
+++ clearshit.lua
@@ -0,0 +1,15 @@
+function table.ClearShit(tbl)
+    local newtable = {}
+    for k,v in pairs(tbl)do
+        if istable(v) then
+            newtable[k] = table.ClearShit(v)
+            continue 
+        end
+        if isfunction(v) or ispanel(v) then
+            newtable[k] = type(v)
+            continue
+        end
+        newtable[k] = v
+    end
+    return newtable
+end
